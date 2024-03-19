a1035580cb5e4859b27775bf0856e53f2b163f7c
Alexey Potory <alexey.potory@gmail.com>
2024-03-19 23:24:37 +0500

    Run command created

---
+++ Autoolbox.App/Commands/RunCommand.cs
@@ -0,0 +1,20 @@
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace Autoolbox.App.Commands;
+
+public sealed class RunCommand : AsyncCommand<RunCommand.Settings>
+{
+    public class Settings : CommandSettings
+    {
+        [CommandArgument(0, "<Config>")] public required string ConfigPath { get; set; }
+        [CommandArgument(1, "<Output>")] public required string OutputPath { get; set; }
+        [CommandArgument(2, "[Count]")] public int Count { get; set; }
+    }
+
+    public override Task<int> ExecuteAsync(CommandContext context, Settings settings)
+    {
+        AnsiConsole.WriteLine("Hello, Run!");
+        return Task.FromResult(0);
+    }
+}
--- Autoolbox.App/Infrastructure/Installers/CommandsInstaller.cs
+++ Autoolbox.App/Infrastructure/Installers/CommandsInstaller.cs
@@ -1,4 +1,5 @@
-﻿using Spectre.Console.Cli;
+﻿using Autoolbox.App.Commands;
+using Spectre.Console.Cli;
 
 namespace Autoolbox.App.Infrastructure.Installers;
 
@@ -6,6 +7,11 @@ public static class CommandsInstaller
 {
     public static IConfigurator AddSonScriptCommands(this IConfigurator configurator)
     {
+        configurator.AddCommand<RunCommand>("run")
+            .WithAlias("r")
+            .WithDescription("Run image generation based on *.sonscript config file")
+            .WithExample("run", "config.sonscript", "images/output", "10");
+
         return configurator;
     }
 }
